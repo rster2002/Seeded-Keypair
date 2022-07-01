ed63c5c1034d3540e74a1b2b291f23cc10436fcc
rster2002 <rster2002dev@gmail.com>
2022-07-01 19:08:45 +0200

    🚧 Working build

--- src/CryptoRSA.ts
+++ src/CryptoRSA.ts
@@ -3,8 +3,9 @@ import RSAGenerator from "./rsa/RSAGenerator";
 export default class CryptoRSA extends RSAGenerator {
     protected randomBit(): boolean {
         let i = new Uint8Array(1);
+        //@ts-ignore
         crypto.getRandomValues(i);
-        
+
         return i[0] % 2 === 0;
     }
 }
